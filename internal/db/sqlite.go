b29cf9b75e567b03474732e81b82f2aa6c140807
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-12-08 18:14:42 +0400

    use .local/share/go-dict-cli for storing dict.db

--- internal/db/sqlite.go
+++ internal/db/sqlite.go
@@ -3,11 +3,28 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"os"
+	"path/filepath"
+
 	_ "github.com/glebarez/go-sqlite"
 )
 
 func Connect() (*sql.DB, error) {
-	db, err := sql.Open("sqlite", "./dict.db")
+
+	home, err := os.UserHomeDir()
+
+	if err != nil {
+		return nil, fmt.Errorf("get user home dir: %w", err)
+	}
+
+	dbPath := filepath.Join(home, ".local", "share", "go-dict-cli", "dict.db")
+
+	err = os.MkdirAll(filepath.Dir(dbPath), 0755)
+	if err != nil {
+		return nil, fmt.Errorf("create db directory: %w", err)
+	}
+
+	db, err := sql.Open("sqlite", dbPath)
 
 	if err != nil {
 		return nil, fmt.Errorf("sql.Open: %w", err)
@@ -25,8 +42,6 @@ func Connect() (*sql.DB, error) {
 		db.Close()
 		return nil, fmt.Errorf("query sqlite_version %w", err)
 	}
-
-	// fmt.Println("- SQL:", sqliteVersion)
 
 	return db, nil
 }
